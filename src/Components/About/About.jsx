a7e95df78ac9c38a1e3b82e3595238e9c1c7e5da
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2023-05-16 12:42:00 +0300

    pfixed

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -11,12 +11,12 @@ const About = () => {
             </div>
             <div className='flex flex-col items-start w-[90%] lg:w-[30rem] lg:ml-[2rem]'>
       
-            <h3 className='flex items-center text-red-600 text-[13px] lg:Text-[15px] font-extrabold italic'><span className='h-[2px] w-[4rem] bg-red-600 mr-2  ' /> Despre Noi</h3>
+            <h3 className='flex items-center text-red-600 text-[13px] lg:text-[15px] font-extrabold italic'><span className='h-[2px] w-[4rem] bg-red-600 mr-2  ' /> Despre Noi</h3>
             <h1 className='text-[20px] lg:text-[32px] font-extrabold text-left'>SCOALA DE SOFERI<span className='text-red-600'> ARAD</span></h1>
             <p className='mt-[1rem] w-[90%] text-[13px] lg:text-[15px]'>
-            Scoala de soferi <span className='font-bold'>AUTO-MOTO POPTEAN</span> suntem cei cu care doresti sa pornesti la drum si ne bucuram sa putem impartasi din experienta noastra viitorilor conducatori auto-moto.</p><p className='mt-[.5rem]'> Prin echipa noastra bine pregatita, oferim servicii de inalta calitatea cursantilor nostri.
+            Scoala de soferi <span className='font-bold'>AUTO-MOTO POPTEAN</span> suntem cei cu care doresti sa pornesti la drum si ne bucuram sa putem impartasi din experienta noastra viitorilor conducatori auto-moto.</p><p className='mt-[.5rem] w-[90%] text-[13px] lg:text-[15px]'> Prin echipa noastra bine pregatita, oferim servicii de inalta calitatea cursantilor nostri.
 Prin serviciile de calitate pe care le oferim, avem un rol esential in pregatirea noilor generatii de conducatori.
-</p><p className='mt-[.5rem]'>
+</p><p className='mt-[.5rem] w-[90%] text-[13px] lg:text-[15px]'>
  Rol care ne obliga la formarea unor viitori conducatori auto-moto capabili sa faca fata oricaror provocari in traficul rutier.
 
             </p>
