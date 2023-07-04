83054d52f55dda777a4f856683ee27d66d5dabf3
Poleac Liviu Stefan <stefan.liviu28^2yahoo.ro>
2023-07-04 17:24:34 +0300

    galeryUpdated

--- src/Components/Galery/Galery.jsx
+++ src/Components/Galery/Galery.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import {AiOutlineRight} from 'react-icons/ai'
-
+import masina1 from '../../Images/masina1.jpg'
+import masina2 from '../../Images/masina2.jpg'
+import masina3 from '../../Images/masina3.jpg'
+import masina4 from '../../Images/masina4.jpg'
+import masina5 from '../../Images/masina5.jpg'
+import scuter from '../../Images/scuter.jpg'
+import moto1 from'../../Images/moto1.jpg'
 const Galery = () => {
     const navigate=useNavigate();
   return (
@@ -13,7 +19,17 @@ const Galery = () => {
                 <a className='cursor-pointer'>Galerie</a>
                 </div>
                 <h2 className='text-[26px] lg:text-[42px] font-extrabold text-center'>VIZUALIZEAZA O PARTE DIN <br/>  <span className='text-red-600'>IMAGINILE NOASTRE</span></h2>
-                
+                <div className='flex flex-col  lg:flex-row flex-wrap justify-between '>
+                <img src={masina1} className='w-[300px] m-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            <img src={scuter} className='w-[300px] m-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            <img src={moto1} className='w-[300px] m-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            <img src={masina2} className='w-[300px] m-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            <img src={masina3} className='w-[300px] m-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            <div>
+            <img src={masina4} className='w-[300px]  m-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            <img src={masina5} className='w-[300px] m-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            </div>
+                  </div>
         </div>
     </div>
   )
--- src/Components/Galery/GaleryMain.jsx
+++ src/Components/Galery/GaleryMain.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { AiOutlineArrowRight } from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom'
 import Fade from 'react-reveal'
+import masina1 from '../../Images/masina1.jpg'
 
+import scuter from '../../Images/scuter.jpg'
+import moto1 from'../../Images/moto1.jpg'
 const GaleryMain = () => {
   const navigate=useNavigate();
   return (
@@ -11,7 +14,11 @@ const GaleryMain = () => {
         <div className='flex flex-col  items-center  lg:items-start justify-between w-[90%] lg:w-[65rem] h-full '>
           <h4 className='flex items-center text-center w-full    text-red-600 text-[13px] lg:text-[15px] font-extrabold italic'><span className='h-[2px] w-[4rem] bg-red-600 mr-2  ' /> Galerie</h4>
           <h3 className='text-[20px] lg:text-[32px] font-extrabold text-center lg:text-left'>CATEVA DIN POZELE NOASTRE</h3>
-          
+          <div className='flex flex-col  lg:flex-row justify-between '>
+            <img src={masina1} className='w-[300px] my-[1rem] lg:my-0 lg:mx-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            <img src={scuter} className='w-[300px] my-[1rem] lg:my-0 lg:mx-[1rem] border-[3px] rounded-[8px] border-red-500' />
+            <img src={moto1} className='w-[300px] my-[1rem] lg:my-0 lg:mx-[1rem] border-[3px] rounded-[8px] border-red-500' />
+          </div>
           
         </div>
       </Fade>
